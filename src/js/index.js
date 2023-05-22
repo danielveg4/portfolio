26afc309165278f5d8cc431dc92b51af5a02ddbe
daniel vega <dvgilles@gmail.com>
2023-05-22 16:43:32 +0200

    menu commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import "../scss/styles.scss";
 
+const toggleMenuElement = document.getElementById('toggle-menu')
+const mainNavElement = document.getElementById('main-nav')
+
+
+function handleClick(){
+    mainNavElement.classList.toggle('hide')
+    
+}
+
+toggleMenuElement.addEventListener('click', handleClick);
+
 const insureMenuElement = document.getElementById("menu-insure");
 const projectInsureElement = document.getElementById("project-insure");
 
